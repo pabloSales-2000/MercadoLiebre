51604c9811bbdb58de3d213ab641b86e03dea3b3
pablo-sales <pablosalesv.9@gmail.com>
2023-08-31 04:50:17 -0300

    Agregue formularios de login y register

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public')
 const PORT = 8080;
 
-app.listen( PORT , () => {
-    console.log(`[servidor]: corriendo en el puerto ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`[servidor]: corriendo en http://localhost:8080/`);
 })
 
-app.use( express.static(publicPath));
+app.use(express.static(publicPath));
 
-app.get( '/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+})
+
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"))
 })
