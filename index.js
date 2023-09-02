fa2ac9969e59d783606f61811aefb17bc0dbac22
pablo-sales <pablosalesv.9@gmail.com>
2023-09-02 14:55:15 -0300

    agregue dudas y problemas

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const publicPath = path.resolve(__dirname, './public')
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`[servidor]: corriendo en el puerto ${PORT}`);
+    console.log(`[servidor]: corriendo en el puerto ${PORT} y http://localhost:8080/`);
 })
 
 app.use(express.static(publicPath));
