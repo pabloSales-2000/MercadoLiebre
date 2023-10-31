c0fbec1a6abf1e49d4b24d2b4a8fb3c88b8de405
pablo-sales <pablosalesv.9@gmail.com>
2023-10-31 18:48:48 -0300

    primeros pasos express

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const publicPath = path.resolve(__dirname, './public')
 const PORT = process.env.PORT || 8080;
+const rutasProductos = require('./src/routers')
 
 app.listen(PORT, () => {
     console.log(`[servidor]: corriendo en el puerto ${PORT} y http://localhost:8080/`);
@@ -10,15 +11,20 @@ app.listen(PORT, () => {
 
 app.use(express.static(publicPath));
 
+app.use('/productos', rutasProductos);
+
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+    res.sendFile(path.resolve(__dirname, "./src/views/home.html"));
 })
 
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/register.html"))
+    res.sendFile(path.join(__dirname, "./src/views/register.html"))
 })
 
-
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/login.html"))
+    res.sendFile(path.join(__dirname, "./src/views/login.html"))
+})
+
+app.get('/productos/:id', (req, res) => {
+    res.send(req.params.id)
 })
