ed2918b6e95eba0421d9e7e1afb93f8875914cd8
pablo-sales <pablosalesv.9@gmail.com>
2023-10-31 19:21:18 -0300

    rutas productos

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const app = express();
 const path = require('path');
 const publicPath = path.resolve(__dirname, './public')
 const PORT = process.env.PORT || 8080;
-const rutasProductos = require('./routers')
+const rutasProductos = require('./routers/productos.js')
 
 app.listen(PORT, () => {
     console.log(`[servidor]: corriendo en el puerto ${PORT} y http://localhost:8080/`);
 })
 
 app.use(express.static(publicPath));
-
-//app.use('/productos', rutasProductos);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
@@ -25,6 +23,4 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/login.html"))
 })
 
-app.get('/productos/:idProducto', (req, res) => {
-    res.send(`Bienvenidos al producto ${req.params.idProducto}`)
-})
+app.use('/productos', rutasProductos);
---
+++ routers/productos.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router;
+
+router.get('/:idProducto', (req, res) => {
+    res.send(`Bienvenidos al producto ${req.params.idProducto}`)
+})
+
+router.get('/:idProducto/comentarios/:idComentario?', (req, res) => {
+    if (req.params.idComentario == undefined) {
+        res.send(`Bienvenido a los comentarios del producto ${req.params.idProducto}`)
+    } else {
+        res.send(`Bienvenido a los comentarios del producto ${req.params.idProducto} y estas enfocado en el comentario ${req.params.idComentario}`)
+    }
+})
+
+module.exports = router;
